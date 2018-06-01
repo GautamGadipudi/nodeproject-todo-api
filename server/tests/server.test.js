392048270bc6219bdd0c27055c545db7f570b2e0
Gautam Gadipudi <gautam.g@deltax.com>
2018-06-01 12:37:07 +0530

    Add test for GET todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -8,8 +8,16 @@ const {
   Todo
 } = require('./../models/todo');
 
+testTodos = [{
+  text: 'Test todo 1'
+}, {
+  text: 'Test todo 2'
+}]
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(testTodos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -28,7 +36,7 @@ describe('POST /todos', () => {
         if (err) {
           return done(err);
         }
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -48,7 +56,7 @@ describe('POST /todos', () => {
           return done(err);
         }
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => {
           done(e);
@@ -56,3 +64,15 @@ describe('POST /todos', () => {
       });
   });
 });
+
+describe('GET/todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  })
+})
