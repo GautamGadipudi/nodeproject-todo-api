170137849307c4982339cc9db4ccfc7b2d553b3d
Gautam Gadipudi <gautam.g@deltax.com>
2018-06-10 22:53:57 +0530

    Add route GET /users/me

--- server/server.js
+++ server/server.js
@@ -113,11 +113,25 @@ app.post('/users', (req, res) => {
   user.save().then(() => {
     return user.generateAuthToken();
   }).then((token) => {
-    res.header('x-token', token).send(user);
+    res.header('x-auth', token).send(user);
   }).catch((e) => {
     res.status(400).send(e);
   });
 });
+
+app.get('/users/me', (req, res) => {
+  var token = req.header('x-auth');
+
+  User.findByToken(token).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    res.send(user);
+  }).catch((e) => {
+    res.status(401).send();
+  })
+})
 
 app.listen(port, () => {
   console.log('Listening on port: ' + port);
